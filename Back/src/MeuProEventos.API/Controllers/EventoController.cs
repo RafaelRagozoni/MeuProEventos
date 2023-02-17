55894bea1602715d7f1726cc867b6073a4f0cbd9
Rafael Ragozoni Conrado <rafael.r.conrado11@hotmail.com>
2023-02-17 15:08:15 -0300

    commit

--- Back/src/MeuProEventos.API/Controllers/EventoController.cs
+++ Back/src/MeuProEventos.API/Controllers/EventoController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MeuProEventos.API.Models;
+using MeuProEventos.API.Data;
 
 namespace MeuProEventos.API.Controllers
 {
@@ -13,42 +14,23 @@ namespace MeuProEventos.API.Controllers
     public class EventoController : ControllerBase
     {
 
-        public IEnumerable<Evento> _evento = new Evento[]{
-            new Evento(){
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                Lote = "1° Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemURL = "foto.png"
-            },
-            new Evento(){
-                EventoId = 2,
-                Tema = "Angular e novidades",
-                Local = "São Paulo",
-                Lote = "1° Lote",
-                QtdPessoas = 50,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemURL = "foto.png"
-            }
-        };
 
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-           
+           _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
         
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPost]
